a0f3e966fafc9a4aada07d7dc2e20cd663e7962b
Bas Wijnen <wijnen@debian.org>
2008-06-04 21:03:34 +0200

    initial work

---
+++ asm.cc
@@ -0,0 +1,328 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <shevek/iostring.hh>
+#include <shevek/args.hh>
+
+struct Param
+{
+	std::string name;
+	bool is_enum;
+	std::map <std::string, unsigned> enum_values;
+	int num_lowest, num_highest;
+
+	bool is_active;
+	int value;
+
+	static void reset ();
+	static Param *find (std::string const &name);
+};
+
+std::list <Param> params;
+
+void Param::reset ()
+{
+	for (std::list <Param>::iterator
+			i = params.begin (); i != params.end (); ++i)
+		i->is_active = false;
+}
+
+Param *Param::find (std::string const &name)
+{
+	for (std::list <Param>::iterator
+			i = params.begin (); i != params.end (); ++i)
+		if (i->name == name)
+			return &*i;
+	return NULL;
+}
+
+struct Source
+{
+	std::list <std::pair <std::string, Param *> > parts;
+	std::string post;
+	std::list <std::string> targets;
+};
+
+std::list <Source> sources;
+
+unsigned ln;
+unsigned addr;
+
+std::string escape (std::string const &in)
+{
+	std::string out;
+	for (unsigned i = 0; i < in.size (); ++i)
+	{
+		if (in[i] == '%')
+			out += "%%";
+		else
+			out += in[i];
+	}
+	return out;
+}
+
+void read_input (std::istream &file)
+{
+	bool is_enum = false, is_num = false, is_source = false;
+	std::map <std::string, int>::iterator current_enum;
+	int current_value;
+	ln = 0;
+	while (std::getline (file, line))
+	{
+		++ln;
+		shevek::istring l (line);
+		l (" ");
+		if (l ("#"))
+			continue;
+		std::string d;
+		if (l ("enum: %s %", d))
+		{
+			is_num = false;
+			is_enum = false;
+			is_source = false;
+			if (Param::find (d))
+			{
+				error (shevek::ostring ("duplicate definition "
+							"of param %s", d));
+				continue;
+			}
+			is_enum = true;
+			params.push_back (Param ());
+			params.back ().name = d;
+			params.back ().is_enum = true;
+			current_enum = params.back ().enum_values.end ();
+			current_value = 0;
+		}
+		else if (l ("name: %l", d))
+		{
+			is_num = false;
+			is_source = false;
+			if (!is_enum)
+			{
+				error ("name without enum");
+				continue;
+			}
+			if (Param::find (escape (d)))
+			{
+				error (shevek::ostring ("duplicate name in param: %s", d));
+				continue;
+			}
+			current_enum = params.back ().enum_values.insert (std::make_pair (escape (d), current_value++)).first;
+		}
+		else if (l ("value: %l", d))
+		{
+			is_num = false;
+			is_source = false;
+			if (!is_enum || current_enum == params.back ().enum_values.end ())
+			{
+				error ("value without enum");
+				continue;
+			}
+			current_enum->second = read_expr (d);
+			current_enum = params.back ().enum_values.end ();
+		}
+		else if (l ("num: %l", d))
+		{
+			is_num = false;
+			is_enum = false;
+			is_source = false;
+			if (Param::find (d))
+			{
+				error (shevek::ostring ("duplicate definition "
+							"of param %s", d));
+				continue;
+			}
+			is_num = true;
+			params.push_back (Param ());
+			params.back ().name = d;
+			params.back ().is_enum = false;
+			params.back ().lowest = 0;
+			params.back ().highest = 0;
+		}
+		else if (l ("lowest: %l", d))
+		{
+			is_enum = false;
+			is_source = false;
+			if (!is_num)
+			{
+				error ("lowest without num");
+				continue;
+			}
+			params.back ().lowest = read_expr (d);
+		}
+		else if (l ("highest: %l", d))
+		{
+			is_enum = false;
+			is_source = false;
+			if (!is_num)
+			{
+				error ("highest without num");
+				continue;
+			}
+			params.back ().highest = read_expr (d);
+		}
+		else if (l ("source: %l", d))
+		{
+			is_num = false;
+			is_enum = false;
+			is_source = true;
+			sources.push_back (Source ());
+			std::string::size_type pos = 0;
+			while (true)
+			{
+				// find next param
+				std::string::size_type first = d.size ();
+				Param *firstp;
+				for (std::list <Param>::iterator i = params.begin (); i != params.end (); ++i)
+				{
+					std::string::size_type p = d.find (i->name, pos);
+					if (p < first)
+					{
+						first = p;
+						firstp = &*i;
+					}
+				}
+				if (first >= d.size ())
+					break;
+				sources.back ().parts.push_back (std::make_pair (escape (d.substr (pos, first - pos)), firstp));
+				pos = first + firstp->name.size ();
+			}
+			sources.back ().post = escape (d.substr (pos));
+		}
+		else if (l ("target: %l", d))
+		{
+			is_num = false;
+			is_enum = false;
+			if (!is_source)
+			{
+				error ("target without source");
+				continue;
+			}
+			sources.back ().targets.push_back (d);
+		}
+		else
+			error (shevek::ostring ("syntax error trying to "
+						"parse `%s'", line));
+	}
+}
+
+void write_out (Source *s)
+{
+	for (std::list <std::string>::iterator
+			i = s->targets.begin (); i != s->targets.end (); ++i)
+	{
+		unsigned byte = read_expr (*i);
+		if (byte < -0x80 || byte >= 0x100)
+			error ("byte out of range");
+		std::cout << (char)byte;
+	}
+}
+
+void parse (std::string line, bool output)
+{
+	shevek::istring l (line);
+	for (std::list <Source>::iterator
+			s = sources.begin (); s != sources.end (); ++s)
+	{
+		l.reset ();
+		l (" ");
+		std::list <std::pair <std::string, Param *> >::iterator p;
+		for (p = s->parts.begin (); p != s->parts.end (); ++p)
+		{
+			if (!l (p->first))
+				break;
+			if (p->second->is_enum)
+			{
+				for (std::map <std::string, unsigned>::iterator v = p->second->enum_values.begin (); v != p->second->enum_values.end (); ++v)
+				{
+					if (!l (escape (v->first)))
+						continue;
+					p->second->value = v->second;
+				}
+				break;
+			}
+			else
+			{
+				std::string::size_type pos = 0;
+				p->second->value = read_expr (l.rest (), pos);
+				if (pos == std::string::npos)
+					break;
+				l.skip (pos);
+			}
+		}
+		if (p != s->parts.end () || !l (s->post) || !l (" %"))
+			continue;
+		if (output)
+			write_out (*s);
+		addr += s->targets.size ();
+		return;
+	}
+}
+
+int main (int argc, char **argv)
+{
+	std::string defs;
+	shevek::args::option opts[] = {
+		shevek::args::option (0, "defs", "code definitions",
+				false, defs),
+	};
+	shevek::args args (argc, argv, opts, 0, 0, "Generic assembler", "2008");
+	bool need_header = false;
+	if (defs.empty ())
+	{
+		defs = argv[0];
+		need_header = true;
+	}
+	std::ifstream file (defs.c_str ());
+	if (!file)
+		shevek_error_errno ("unable to open file " << defs);
+	std::string line;
+	if (need_header)
+	{
+		std::getline (file, line);
+		if (!file || line.size () < 2
+				|| line[0] != '#' || line[1] != '!')
+			shevek_error ("you must specify a definitions file");
+	}
+	read_input (file);
+	if (errors)
+		return 1;
+	std::vector <std::pair <unsigned, std::string> > input;
+	std::string pre;
+	ln = 0;
+	while (std::getline (std::cin, line))
+	{
+		++ln;
+		if (!line.empty () && line[line.size () - 1] == '\\')
+		{
+			pre += line.substr (0, line.size () - 1);
+			continue;
+		}
+		input.push_back (std::make_pair (ln, pre + line));
+		pre.clear ();
+	}
+	if (!pre.empty ())
+		input.push_back (pre);
+	// Determine labels
+	addr = 0;
+	for (unsigned t = 0; t < input.size (); ++t)
+	{
+		ln = input[t].first;
+		parse (input[t].second, false);
+	}
+	if (errors)
+		return 1;
+	// Write output
+	addr = 0;
+	for (unsigned t = 0; t < input.size (); ++t)
+	{
+		ln = input[t].first;
+		parse (input[t].second, true);
+	}
+	if (errors)
+		return 1;
+	return 0;
+}
