ec63dacaef347848fb98d34aada0e4c237807eab
Bas Wijnen <shevek@led.(none)>
2008-11-11 22:59:25 +0100

    allow strings in defb

--- src_asm/dir_defb.cc
+++ src_asm/dir_defb.cc
@@ -1,5 +1,48 @@
 #include "asm.hh"
 
+static std::list <int> defb_expr (shevek::istring &args, unsigned &undef)
+{
+	std::list <int> ret;
+	args (" ");
+	if (args.rest ().empty ())
+	{
+		error ("empty expression in defb");
+		return std::list <int> ();
+	}
+	Glib::ustring r = args.rest ();
+	if (r[0] == '\'' || r[0] == '"')
+	{
+		Glib::ustring::size_type p = 1;
+		while (p < r.size () && r[p] != r[0])
+		{
+			ret.push_back (r[p]);
+			++p;
+		}
+		if (p >= r.size ())
+		{
+			error ("unterminated string in defb");
+			return std::list <int> ();
+		}
+		args.skip (p + 1);
+	}
+	else
+	{
+		Glib::ustring::size_type pos = 0;
+		bool valid;
+		int v = read_expr (args.rest (), false, pos, &valid);
+		if (pos == Glib::ustring::npos)
+		{
+			error ("invalid expression in defb");
+			return std::list <int> ();
+		}
+		if (!valid)
+			++undef;
+		args.skip (pos);
+		ret.push_back (v);
+	}
+	return ret;
+}
+
 unsigned dir_defb (shevek::istring &args, bool write, bool first,
 		Label *current_label)
 {
@@ -8,22 +51,19 @@ unsigned dir_defb (shevek::istring &args, bool write, bool first,
 	unsigned s = 0;
 	while (true)
 	{
-		bool valid;
-		Glib::ustring::size_type pos = 0;
-		int v = read_expr (args.rest (), false, pos, &valid);
-		if (pos == Glib::ustring::npos)
-		{
-			error ("invalid expression");
+		std::list <int> v = defb_expr (args, undef);
+		if (v.empty ())
 			break;
+		if (write)
+		{
+			std::list <int>::iterator i;
+			unsigned p;
+			for (i = v.begin (), p = 0; i != v.end (); ++i, ++p)
+				write_byte (*i, s + p);
 		}
-		args.skip (pos);
+		s += v.size ();
 		if (!args (" ,"))
 			break;
-		if (!valid)
-			++undef;
-		if (write)
-			write_byte (v, s);
-		++s;
 	}
 	addr += s;
 	if (write && listfile)
